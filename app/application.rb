c38fdf3ed50a4b08a087bfd511407111bb40254e
Joseph Parker <jparker010190@gmail.com>
2018-05-06 03:47:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,8 @@ class Application
     elsif req.path.match(/add/)
       search_term = req.params["item"]
       resp.write handle_search(search_term)
+    else 
+      resp.write "Your cart is empty"
     end
     resp.finish
   end
